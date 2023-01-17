e8d2a3d419518549437635b7e57e14bb40404e34
mad-of-gravity <mad.of.gravity@gmail.com>
2023-01-17 07:55:22 +0200

    adding tag class

--- src/Tags.js
+++ src/Tags.js
@@ -3,7 +3,7 @@ function Tags({tags = [ "boomdotdev", "task", "tags", "react" ]}) {
     return (
         <div className=".tags">
             {tags.map((tag, index) => {
-                return <span key={index}>#{tag}</span>
+                return <span className="tag" key={index}>#{tag}</span>
             })}
         </div>
     )
