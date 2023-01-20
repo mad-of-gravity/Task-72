ff2f465c0a2fb4be8d0ed38c65f519aca5dae2fc
mad-of-gravity <mad.of.gravity@gmail.com>
2023-01-20 17:51:57 +0200

    fix validation error

--- src/Tags.js
+++ src/Tags.js
@@ -2,10 +2,10 @@ function Tags({tags}) {
     
     return (
         <div className="tags">
-            #{<span className="tag">{tags[0]}</span>}
-            #{<span className="tag">{tags[1]}</span>}
-            #{<span className="tag">{tags[2]}</span>}
-            #{<span className="tag">{tags[3]}</span>}
+            <span className="tag">#{tags[0]}</span>
+            <span className="tag">#{tags[1]}</span>
+            <span className="tag">#{tags[2]}</span>
+            <span className="tag">#{tags[3]}</span>
         </div>
     )
 }
